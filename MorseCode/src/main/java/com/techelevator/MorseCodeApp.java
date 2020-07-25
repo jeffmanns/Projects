32ecc7d6b6ed784228c8c79ab8c5b984f2829fe3
Jeffrey Manns <jeff.t.manns@gmail.com>
2020-07-25 12:59:16 -0400

    added scanner to read File Input and save contents to a list of MorseCode Objects

--- MorseCode/src/main/java/com/techelevator/MorseCodeApp.java
+++ MorseCode/src/main/java/com/techelevator/MorseCodeApp.java
@@ -1,10 +1,37 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MorseCodeApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
 
+		List<MorseCode> morseCodeList = new ArrayList<>();
+
+		// READ INPUT FROM MORSE CODE TEXT FILE
+		File inputTextFile = new File("MorseCodeValues.txt");
+
+		try (Scanner fileScanner = new Scanner(inputTextFile)) {
+
+			while (fileScanner.hasNextLine()) {
+				String[] lineInput = fileScanner.nextLine().split(",");
+				MorseCode temp = new MorseCode(lineInput[0], lineInput[1]);
+				morseCodeList.add(temp);
+			}
+		}
+		
+//		for(MorseCode m : morseCodeList) {
+//			System.out.println(m.getCharacters() + " " + m.getDotsAndDashes());
+//		}
+		
+		// READ INPUT FROM USER
+		
+		
+		
+		
 	}
-
 }
