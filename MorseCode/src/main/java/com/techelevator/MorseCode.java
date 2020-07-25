e12e3fcf1da59444eead4e8d8c4336937ac42693
Jeffrey Manns <jeff.t.manns@gmail.com>
2020-07-25 12:08:53 -0400

    Created MorseCode model class, morse code comma-delimited text file, and MorseCodeApp class with getters, setters, comnstructors, and data members

---
+++ MorseCode/src/main/java/com/techelevator/MorseCode.java
@@ -0,0 +1,38 @@
+package com.techelevator;
+
+public class MorseCode {
+	
+	//DATA MEMBERS
+	
+	private String characters;
+	private String dotsAndDashes;
+	
+	//CONSTRUCTORS
+	
+	public MorseCode(String characters, String dotsAndDashes) {
+		this.characters = characters;
+		this.dotsAndDashes= dotsAndDashes;
+	}
+
+	
+	//GETTERS AND SETTERS
+	
+	public String getCharacters() {
+		return characters;
+	}
+	
+	public void setCharacters(String characters) {
+		this.characters = characters;
+	}
+	
+	public String getDotsAndDashes() {
+		return dotsAndDashes;
+	}
+	
+	public void setDotsAndDashes(String dotsAndDashes) {
+		this.dotsAndDashes = dotsAndDashes;
+	}
+	
+	//OTHER METHODS
+
+}
