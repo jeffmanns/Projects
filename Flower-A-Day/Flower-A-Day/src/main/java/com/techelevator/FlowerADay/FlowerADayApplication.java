7bbcb884ea15de34a7b4e0398d7531ac2fec07da
Jeffrey Manns <jeff.t.manns@gmail.com>
2020-07-27 21:43:39 -0400

    got GET request working correctly and returning an array of JSON objects from external API representing a set of flower photos. Next step is to search through that array to automatically select a single object that represents a single photo of a flower

--- Flower-A-Day/Flower-A-Day/src/main/java/com/techelevator/FlowerADay/FlowerADayApplication.java
+++ Flower-A-Day/Flower-A-Day/src/main/java/com/techelevator/FlowerADay/FlowerADayApplication.java
@@ -10,28 +10,28 @@ public class FlowerADayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlowerADayApplication.class, args);
 
+		Flower flower = new Flower();
+		
+		final String API_BASE_URL = "https://pixabay.com/api/?key=17646055-e0e93f137cbae14c739247758&q=";
+		RestTemplate restTemplate = new RestTemplate();
+		
+//	public static Flower[] searchAllFlowers() {
+//		return restTemplate.getForObject(API_BASE_URL + "flowers&image_type=photo", Flower[].class);
+//	}
+//	
+//	public static Flower[] searchFlowersByColor() {
+//		return restTemplate.getForObject(API_BASE_URL + flower.getRandomColor() + "+flowers" + "&image_type=photo", Flower[].class);
+//	}
+//	
+//	public static Flower[] searchFlowersByType() {
+//		return restTemplate.getForObject(API_BASE_URL + flower.getRandomFlowerType() + "+flowers" + "&image_type=photo", Flower[].class);
+//	}
+		
+		//public static Flower[] searchFlowersByColorAndType() {
+			String response = restTemplate.getForObject(API_BASE_URL + flower.getRandomColor() + "+" + flower.getRandomFlowerType() + "+flowers" + "&image_type=photo", String.class);
+		
 	}
 	
-	Flower flower = new Flower();
-	
-	private static final String API_BASE_URL = "https://pixabay.com/api/?key=17646055-e0e93f137cbae14c739247758&q=";
-	private static RestTemplate restTemplate = new RestTemplate();
-	
-	public static Flower[] searchAllFlowers() {
-		return restTemplate.getForObject(API_BASE_URL + "flowers&image_type=photo", Flower[].class);
-	}
-	
-	public static Flower[] searchFlowersByColor() {
-		return restTemplate.getForObject(API_BASE_URL + flower.getRandomColor() + "+flowers" + "&image_type=photo", Flower[].class);
-	}
-	
-	public static Flower[] searchFlowersByType() {
-		return restTemplate.getForObject(API_BASE_URL + flower.getRandomFlowerType() + "+flowers" + "&image_type=photo", Flower[].class);
-	}
-	
-	public static Flower[] searchFlowersByColorAndType() {
-		return restTemplate.getForObject(API_BASE_URL + flower.getRandomColor() + "+" + flower.getRandomFlowerType() + "+flowers" + "&image_type=photo", Flower[].class);
-	}
 	
 
 }
